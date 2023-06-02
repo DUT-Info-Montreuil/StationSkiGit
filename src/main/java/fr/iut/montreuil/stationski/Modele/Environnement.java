b2fe6d1489ac598a4554220e1c382b087e47cfbd
nareio <much.lio@laposte.net>
2023-06-02 14:18:35 +0200

    fix trop argent

--- src/main/java/fr/iut/montreuil/stationski/Modele/Environnement.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/Environnement.java
@@ -22,7 +22,7 @@ public class Environnement {
         this.terrain = terrain;
         this.vague = new Vague(1, 100,6,9,0,this);
         this.listeTours = FXCollections.observableArrayList();
-        this.argent = new SimpleIntegerProperty(10500);
+        this.argent = new SimpleIntegerProperty(500);
         this.PV = new SimpleIntegerProperty(20);
         //this.tour = 0;
         this.nbEnnemis = new SimpleIntegerProperty(this.vague.getListEnnemis().size());
