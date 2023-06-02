f0652b6be6711714adf50585bcb4a87b70177c5f
Trupin <120423796+Matthias-iut@users.noreply.github.com>
2023-06-02 11:30:09 +0200

    Ajout d'une BarreDeVie sur chaque Ennemi touché

--- src/main/java/fr/iut/montreuil/stationski/Controleur/ListObs.java
+++ src/main/java/fr/iut/montreuil/stationski/Controleur/ListObs.java
@@ -3,12 +3,17 @@ package fr.iut.montreuil.stationski.Controleur;
 import fr.iut.montreuil.stationski.Main;
 import fr.iut.montreuil.stationski.Modele.Entite;
 import fr.iut.montreuil.stationski.Modele.Environnement;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.ListChangeListener;
 import fr.iut.montreuil.stationski.Modele.*;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 
 import java.net.URL;
 
@@ -27,22 +32,46 @@ public class ListObs implements ListChangeListener<Entite> {
         while(c.next()){
 
             for(Entite e : c.getAddedSubList()){
-
+                ChangeListener<Number> listenPV = (
+                        (obs, old, nouv) -> {if (nouv.intValue()>0 && old.intValue()==e.getPVMax()) creerBarreDeVie(e);}
+                );
+                e.pvProperty().addListener(listenPV);
                 creerSprite(e);
             }
 
             for(Entite  e : c.getRemoved()){
                 Node sprite = panneauJeu.lookup("#" + e.getId());
                 panneauJeu.getChildren().remove(sprite);
-            }
+                sprite = panneauJeu.lookup("#"+e.getId()+"R");
+                panneauJeu.getChildren().remove(sprite);
 
+            }
         }
     }
-
+    public void creerBarreDeVie (Entite e){
+        Rectangle rectangle = new Rectangle();
+        ChangeListener<Number> listenerX = (
+                (obs, old, nouv) -> {rectangle.setTranslateX(nouv.floatValue());}
+        );
+        ChangeListener<Number> listenerY = (
+                (obs, old, nouv) -> {rectangle.setTranslateY((nouv.floatValue()) -5);}
+        );
+        ChangeListener<Number> listenPVActeur = (
+                (obs, old, nouv) -> {rectangle.setWidth(Math.floor(nouv.floatValue()*12));
+                    System.out.println(rectangle.getWidth());}
+        );
+        e.getPosXP().addListener(listenerX);
+        e.getPosYP().addListener(listenerY);
+        e.pourcentPVProperty().addListener(listenPVActeur);
+        rectangle.setId(e.getId()+"R");
+        rectangle.setWidth(12);
+        rectangle.setHeight(1);
+        rectangle.setFill(Color.GREEN);
+        panneauJeu.getChildren().add(rectangle);
+    }
     public void creerSprite(Entite e){
         URL urlIm;
         if(e instanceof Ennemi){
-
             if (e instanceof SkieurBasique) {
                 urlIm = Main.class.getResource("skieur1.png");
             }
@@ -74,6 +103,7 @@ public class ListObs implements ListChangeListener<Entite> {
             imageEn.setId(e.getId());
             panneauJeu.getChildren().add(imageEn);
         }
+
 
     }
 }
