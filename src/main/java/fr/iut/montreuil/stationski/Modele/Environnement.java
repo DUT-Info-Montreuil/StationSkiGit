28e1950f124ef88c53e3a5471b730068ec402a81
Trupin <120423796+Matthias-iut@users.noreply.github.com>
2023-06-02 11:29:05 +0200

    Update Environnement.java

--- src/main/java/fr/iut/montreuil/stationski/Modele/Environnement.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/Environnement.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Environnement {
     private Terrain terrain;
-   // private int tour;
+    // private int tour;
     private IntegerProperty argent;
     private ArrayList<Integer> listeEnv;
     private Capacite capa;
