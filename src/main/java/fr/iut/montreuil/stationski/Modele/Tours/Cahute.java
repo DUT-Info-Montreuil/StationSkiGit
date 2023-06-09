6c227da82f0a833f7d6ca7619f6c74dedc917a1d
nareio <much.lio@laposte.net>
2023-06-09 12:59:34 +0200

    add comment

--- src/main/java/fr/iut/montreuil/stationski/Modele/Tours/Cahute.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/Tours/Cahute.java
@@ -16,6 +16,7 @@ public class Cahute extends Tour {
     }
 
     public void creerAllier (boolean b){
+        // ATTENTION : les alliers ne spawn que sur la ligne de cahute pas les col actuellement
         if (b) {
             int numcaseori = (getPosY()/16)*45+(getPosX()/16);
             int numcase0 = 0;
