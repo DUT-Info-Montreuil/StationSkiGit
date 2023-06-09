d63aabf79c8587eb67267caa9f2fdc6df485dd43
nareio <much.lio@laposte.net>
2023-06-09 12:10:32 +0200

    cahute : spawn allier sur route (a rectifier)

--- src/main/java/fr/iut/montreuil/stationski/Modele/Tours/Cahute.java
+++ src/main/java/fr/iut/montreuil/stationski/Modele/Tours/Cahute.java
@@ -32,12 +32,10 @@ public class Cahute extends Tour {
                     }
                 }
             }
-            System.out.println(numcase0);
-            System.out.println(numcaseori);
             System.out.println(this.env.getTerrain().getList().get(numcase0));
-            x = (numcase0%45)*16;
-            y= (numcase0/45)*16;
-            System.out.println("x : "+x+"y : "+y);
+            // attenntion, ici 32 pas 45
+            x = (numcase0%32)*16;
+            y= (numcase0/32)*16;
             Allier a1 = new Allier(10, x, y, env);
             Allier a2 = new Allier(50, x, y, env);
             Allier a3 = new Allier(90, x, y, env);
