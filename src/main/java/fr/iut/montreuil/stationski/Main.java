c2a8dd792a7dcb449b7298af52cd2b3680657d45
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2023-06-09 15:45:49 +0200

    maj du menu : police, taille

--- src/main/java/fr/iut/montreuil/stationski/Main.java
+++ src/main/java/fr/iut/montreuil/stationski/Main.java
@@ -4,6 +4,7 @@ package fr.iut.montreuil.stationski;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +12,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(Main.class.getResource("flash/Flash.ttf").toExternalForm(), 50);
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),  1000, 800);
         stage.setTitle("StationSki");
