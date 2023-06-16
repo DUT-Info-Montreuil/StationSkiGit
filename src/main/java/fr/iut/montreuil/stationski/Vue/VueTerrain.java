8e739a6568b4fe5216859bf5dd3873eefca6450f
Baptiste-B14 <blanchet.baptiste77@gmail.com>
2023-06-16 10:48:57 +0200

    correction path file csv terrain

--- src/main/java/fr/iut/montreuil/stationski/Vue/VueTerrain.java
+++ src/main/java/fr/iut/montreuil/stationski/Vue/VueTerrain.java
@@ -63,10 +63,10 @@ public class VueTerrain {
         URL url= Main.class.getResource("/fr/iut/montreuil/stationski/TileSet_Final_CSV.csv");
         String path;
 
-        if(this.intMapSelect==0) path = "C:/Users/matth/Documents/GitHub/StationSkiGit/target/classes/fr/iut/montreuil/stationski/TileSet_Final_CSV.csv";
-        else if (this.intMapSelect==1) path = "C:/Users/matth/Documents/GitHub/StationSkiGit/target/classes/fr/iut/montreuil/stationski/TileSet2_Final_CSV.csv";
-        else if (this.intMapSelect==2) path = "C:/Users/matth/Documents/GitHub/StationSkiGit/target/classes/fr/iut/montreuil/stationski/TileSet3_Final_CSV.csv";
-        else path = "C:/Users/matth/Documents/GitHub/StationSkiGit/target/classes/fr/iut/montreuil/stationski/TileSet4_Final_CSV.csv";
+        if(this.intMapSelect==0) path = "src/main/resources/fr/iut/montreuil/stationski/TileSet_Final_CSV.csv";
+        else if (this.intMapSelect==1) path = "src/main/resources/fr/iut/montreuil/stationski/TileSet2_Final_CSV.csv";
+        else if (this.intMapSelect==2) path = "src/main/resources/fr/iut/montreuil/stationski/TileSet3_Final_CSV.csv";
+        else path = "src/main/resources/fr/iut/montreuil/stationski/TileSet2_Final_CSV.csv";
         String line="";
         int i=0;
         try{
